d1ee42e7ffef77737800a05d701f540bf5cc948c
wafa <wafa>
2017-11-08 19:50:26 +0100

    save files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<button (click)="getall()">GET</button>
+  <ul>
+    <li *ngFor="let i of res">{{i.titre}}</li>
+  </ul>
+    <fieldset>
+    <legend>Composant Pere = </legend>
+    <p>La valeur de fils: </p>
+    La valeur saisie par le Pere:<input type="text">
+    <app-fils></app-fils>
+  </fieldset>`
+
+
 })
 export class AppComponent {
-  title = 'app works!';
+  res: any[];
+  constructor(private http: Http) {
+    // console.log(this.res);
+  }
+  getall() {
+    this.http.get('../assets/movies.json').subscribe(
+      res => this.res = res.json());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes , RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PersonneComponent } from './Personne/personne.component';
+import { ListComponent } from './Etudiant/list.component';
+import { DetailComponent } from './Etudiant/detail.component';
+import { AccueilComponent } from './Etudiant/accueil.component';
+import { FilsComponent } from './fils.component';
 
+// const listPath: Routes = [
+//   {path: '' , redirectTo: 'list' , pathMatch: 'full' },
+//   {path: 'accueil' , component: AccueilComponent },
+//   {path: 'list' , component: ListComponent },
+//   {path: 'detail' , component: DetailComponent },
+//   {path: '**' , component: AccueilComponent }
+//
+// ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonneComponent,
+    ListComponent,
+    DetailComponent,
+    AccueilComponent,
+    FilsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    HttpModule,
+    FormsModule
+    // RouterModule.forRoot(listPath)
   ],
   providers: [],
   bootstrap: [AppComponent]
